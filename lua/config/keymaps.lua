0d66f336f0482f15adb4b4e9523b67f20e4d0f17
Ain Ruiz Dorado <ain.ruiz.dorado@gmail.com>
2023-07-15 10:50:47 -0300

    Nuevos keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local Util = require("lazyvim.util")
+
 -- Shorten function name
 local function map(mode, lhs, rhs, opts)
   local keys = require("lazy.core.handler").handlers.keys
@@ -17,19 +19,30 @@ local function map(mode, lhs, rhs, opts)
   end
 end
 
+-- Sadly I have to delete the default keymapings to this thing to work, lazyvim is planned to have default mappings that not apply to my workflow
 for _, key in pairs({
   "<leader>wd", -- delete window, <C-W>c, now just quit
   "<leader>ww", -- other window, <C-W>p, not necessary
   "<leader>w-", -- duplicate split window <C-W>s
   "<leader>w|", -- duplicate split window <C-W>v
   "<leader>qq", -- quit all
+  "<leader><tab>[",
+  "<leader><tab>]",
 }) do
   vim.keymap.del("n", key)
 end
 
+-- faster quit and save
 map("n", "<leader>q", "<cmd>q<cr>", { desc = "Faster quit" })
-
 map("n", "<leader>w", "<cmd>w<cr><esc>", { desc = "Faster Write" })
 
-map("n", "<leader><tab>}", "<cmd>tabnext<cr>", { desc = "Next Tab" })
-map("n", "<leader><tab>{", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+-- Tabs Handler
+map("n", "<A-.>", "<cmd>tabnext<cr>")
+map("n", "<A-,>", "<cmd>tabprevious<cr>")
+
+-- terminal handler
+local lazyterm = function()
+  Util.float_term(nil, { cwd = Util.get_root() })
+end
+map("n", "<C-r>", lazyterm, { desc = "Terminal (root dir)" })
+map("t", "<C-r>", "<cmd>close<cr>", { desc = "Hide Terminal" })
