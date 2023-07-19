fd49e58f782b483391009857f74a6e92c764dca5
Ain Ruiz Dorado <ain.ruiz.dorado@gmail.com>
2023-07-19 00:03:08 -0300

    Se agrego y configuro Colorizer

--- init.lua
+++ init.lua
@@ -6,3 +6,8 @@ opt.termguicolors = true
 vim.api.nvim_set_option("termguicolors", true)
 opt.cursorline = false -- Enable highlighting of the current line
 opt.expandtab = false -- Use spaces instead of tabs
+
+vim.cmd([[set shell=powershell]])
+vim.cmd([[set shellcmdflag=-command]])
+vim.cmd([[set shellquote=\"]])
+vim.cmd([[set shellquote=]])
