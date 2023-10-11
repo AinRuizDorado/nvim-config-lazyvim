bcf92e77762b475245e9b74f167dae633e864b3a
Ain Ruiz Dorado <ain.ruiz.dorado@gmail.com>
2023-10-11 15:07:27 -0300

    nuev

---
+++ lua/plugins/tressiter.lua
@@ -0,0 +1,33 @@
+return {
+
+  {
+
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
+      ensure_installed = {
+        "bash",
+        "java",
+        "c",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+      },
+    },
+  },
+}
